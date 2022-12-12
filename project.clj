eae535dbfde2229ee281b2899a5bc9e3d826f434
Bingen Galartza Iparragirre <bingen.galartza@magnet.coop>
2022-12-12 14:50:24 +0100

    Fix license, url and description in project.clj

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject dev.gethop/timbre.appenders.cloudwatch "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :description "Duct module that provides a Cloudwatch Timbre log appender"
+  :url "https://github.com/gethop-dev/timbre.appenders.cloudwatch"
+  :license {:name "Mozilla Public License 2.0"
+            :url "https://www.mozilla.org/en-US/MPL/2.0/"}
   :dependencies [[org.clojure/clojure "1.10.3"]
                  [org.clojure/core.async "1.5.648"]
                  [diehard "0.11.6"]
