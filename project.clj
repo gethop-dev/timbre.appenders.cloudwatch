38941c97d57e6e98fb1f4a99b07a56f90f75b168
Bingen Galartza Iparragirre <bingen.galartza@magnet.coop>
2022-12-12 14:49:26 +0100

    Update dependencies

--- project.clj
+++ project.clj
@@ -5,14 +5,14 @@
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.3"]
                  [org.clojure/core.async "1.5.648"]
-                 [diehard "0.11.3"]
+                 [diehard "0.11.6"]
                  [integrant "0.8.0"]
-                 [com.cognitect.aws/api "0.8.532"]
-                 [com.cognitect.aws/endpoints "1.1.12.206"]
-                 [com.cognitect.aws/logs "821.2.1107.0"]
-                 [clojure.java-time "0.3.3"]
-                 [metosin/jsonista "0.3.5"]
-                 [camel-snake-kebab "0.4.2"]]
+                 [com.cognitect.aws/api "0.8.630"]
+                 [com.cognitect.aws/endpoints "1.1.12.358"]
+                 [com.cognitect.aws/logs "825.2.1262.0"]
+                 [clojure.java-time "1.1.0"]
+                 [metosin/jsonista "0.3.7"]
+                 [camel-snake-kebab "0.4.3"]]
   :repl-options {:init-ns dev.gethop.timbre.appenders.3rd-party.cloudwatch}
   :deploy-repositories [["snapshots" {:sign-releases false
                                       :url "https://clojars.org/repo"
@@ -27,8 +27,8 @@
    :repl {:repl-options {:host "0.0.0.0"
                          :port 4002}}
    :profiles/dev {}
-   :project/dev {:plugins [[jonase/eastwood "1.2.3"]
-                           [lein-cljfmt "0.8.0"]]
+   :project/dev {:plugins [[jonase/eastwood "1.3.0"]
+                           [lein-cljfmt "0.9.0"]]
                  :eastwood {:linters [:all]
                             :source-paths ["src"]
                             :test-paths ["test"]
